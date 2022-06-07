905fb1f712e74ea58dd4ffc60e357514cb68da28
name <nameric@ymail.com>
2022-06-06 21:59:38 -0700

    added comments in java script for whats needs to be done.

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-// Generate Password
-function generatePassword() {}
+//storing all needed characters in variables
 
+//numbers
+var numbers = ["1", "2", "3", "4"];
+
+//special characters
+var specialCharacters = ["!", "@", "#"];
+
+//lower case letters
+var specialCharacters = ["a", "b", "c"];
+
+//uppercase letters
+
+//What sorts of tasks does my code need to complete
+
+// Generate Password
+function generatePassword() {
+  // Prompt the user for their passowrd length and store it in a variable
+  //Validate the password length and make sure it's a number between or equal to 8 and 128
+  // Prompt the user for if they want special characters and store it in a variable
+  //@see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+}
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
