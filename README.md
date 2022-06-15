5149047a0326ac982ba1d15bd719388ebbd0a182
name <nameric@ymail.com>
2022-06-15 05:00:55 -0700

    cleaned up the comments in js.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Links
 
 - https://www.w3schools.blog/string-length-validation-javascript-js
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
--- script.js
+++ script.js
@@ -85,13 +85,11 @@ var upperCase = [
   "Z",
 ];
 
-//What sorts of tasks does my code need to complete
-
 // Generate Password
 function generatePassword() {
   // 'Prompt' the user for their 'passwordLength' and store it in a variable
-  var length = prompt("Choose number of characters you want in your password");
 
+  var passwordLength;
   var userNumber;
   var userSpecialcharacter;
   var userLowercase;
@@ -99,13 +97,15 @@ function generatePassword() {
 
   //Validate the password length and make sure it's a number between or equal to 8 and 128
   //IF 'passwordLength' greater than or equal to 8 and 'passwordLength' less than or equal to 128
-
-  if (length >= 8 && length <= 128) {
-    // Prompt the user for if they want special characters and store it in a variable
+  var passwordLength = prompt(
+    "Choose number of characters you want in your password"
+  );
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    // Prompt the user for if they want numbers and store it in a variable
 
     userNumber = confirm("Do you want to include numbers?");
 
-    // Prompt the user for if they want numbers and store it in a variable
+    // Prompt the user for if they want special characters and store it in a variable
 
     userSpecialcharacter = confirm(
       "Do you want to include Special characters?"
@@ -121,13 +121,13 @@ function generatePassword() {
   }
   //IF 'passwordLength' less than 8 OR greater than 128;
   //end the function early: return;
-  else if (length < 8 || length > 128) {
-    alert("Number of characters must be between or equal to 8 to 128");
+  else if (passwordLength < 8 || passwordLength > 128) {
+    alert("Number of characters must be between or equal to 8 and 128");
     return "";
   }
 
   // validate that at least one character choice is true
-  // if (!storeCharacter && !storeNumber && !storeLowercase && !storeUppercase)
+  // if (!userCharacter && !userNumber && !userLowercase && !userUppercase)
   // -Alert the user they had bad data (need to choose at least one character)
   //  -return "";
 
@@ -142,47 +142,42 @@ function generatePassword() {
   }
 
   //DECLARE a new empty array to store the 'characters' we want to use
-  // var ___ = [];
-
-  // var 1 =[];
-  // var 2 =[];
+  // var (empty array) = [];
   var userChoice = [];
-  var randomPassword = "";
+
   // IF 'specialCharacters' is true
   // combine 'specialCharacters' with 'characters'
-  // ___ = ___.concat(1);
+  // (empty array) = (empty array).concat(character);
   // IF 'numbers' is true
   // ___ = ___.concat(1);
 
   if (userNumber == true) {
     userChoice = userChoice.concat(numbers);
   }
-
   if (userSpecialcharacter == true) {
     userChoice = userChoice.concat(specialCharacters);
   }
-
   if (userLowercase == true) {
     userChoice = userChoice.concat(lowerCase);
   }
-
   if (userUppercase == true) {
     userChoice = userChoice.concat(upperCase);
   }
-
-  //@see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
-
   // DECLARE a new empty string to store the password characters
+  var randomPassword = "";
+
   //for( var i=0; i< 'passwordLength'; i++)
-  for (var i = 0; i < length; i++) {
+  for (var i = 0; i < passwordLength; i++) {
+    //Randomly select characters
+    //Adding that character to a building password string
+
     randomPassword = userChoice[Math.floor(Math.random() * userChoice.length)];
+
     console.log(randomPassword);
+
+    //RETURN the completed string
     return randomPassword;
   }
-
-  //Randomly select characters
-  //Adding that character to a building password string
-  //RETURN the completed string
 }
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
